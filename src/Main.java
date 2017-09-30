6fb3e2ed7fbb1388e000d5dd5d3cd6a4b9458c2e
Nihar Gupte <31829255+iamNihar07@users.noreply.github.com>
2017-09-30 15:14:14 -0500

    Add files via upload
    Reads from file Helloworld.mav and imports it into an array of object Line

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
-import tokenizer.tokenizer;
+package tokenizer;
+import tokenizer.*;
 import tokenizer.tokenizer.Line;
+import java.io.*;
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        tokenizer tok = new tokenizer();         
+        //Line a = tok.get_tree("mav mdicik = 5.6;");
 
-public class Main {
-    public static void main(String[] args) {
-        tokenizer tok = new tokenizer();
-        Line a = tok.get_tree("if (a == b) {");
-        Line b = tok.get_tree("while (adarsh == gay) {");
+        String n; 
+        int k=0;
+        try
+        {
+            FileReader fr=new FileReader("helloworld.mav");
+            BufferedReader in=new BufferedReader(fr);
 
+            //System.out.println("Following are the details in the file:");
+            while((n=in.readLine())!=null)
+            {                
+                k++;
+            }
+            in.close();
+            fr.close();
+
+            Line AST[]=new Line[k];
+            int i=0;
+            fr=new FileReader("helloworld.mav");
+            in=new BufferedReader(fr);
+            while((n=in.readLine())!=null)
+            {
+                Line a=tok.get_tree(n); 
+                AST[i]=a;
+                i++;            
+            }
+            in.close();
+            fr.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            //System.err.println(e);
+        }     
+
+        
+        
         System.out.println("ayyeee");
-
     }
 }
