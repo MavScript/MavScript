bb2b261025371190003066fa3b26d9b2b717b8be
Adarsh Pai <adarsh9pai@gmail.com>
2017-09-30 20:30:57 -0500

    Add files via upload

---
+++ src/tokenizer/cli.java
@@ -0,0 +1,14 @@
+
+package tokenizer;
+import tokenizer.*;
+import java.util.Scanner;
+
+public class cli{
+
+
+        Scanner scanner=new Scanner(System.in);
+        String file_name=scanner.nextLine();
+
+
+
+}
