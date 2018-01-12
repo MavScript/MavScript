d5aaf454d9b7eb457296b11a54a5f4e29437ae95
vardaro <anthonyavardaro@yahoo.com>
2018-01-12 17:13:46 -0600

    arrays are finally implemented

---
+++ examples/array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        int[] myNum = {1, 2, 5, 3, 2, 5};
+        String[] myStr = {"hello", "hi", "hello"};
+        Boolean[] booly = {true, true, false};
+        System.out.println(myNum[1] + myNum[2]);
+    }
+}
